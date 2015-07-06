8f255f462dab95c9a999f0b3fa7998eb42e2f1a5
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2015-07-06 22:45:33 +0300

    Version bump to 0.4.0
    Jump in version to match version on PyPI.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='petroglyph',
-    version='0.3.0',
+    version='0.4.0',
     description='A static blog generator',
     url='https://github.com/polybuildr/petroglyph',
     author='Vivek Ghaisas',
